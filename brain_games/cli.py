1530e7fa15e4d7ca469c7d6352e8e2ba38bb26bf
EvilMadSquirrel <minichev.s.l@gmail.com>
2021-11-01 13:28:33 +0300

    finish step four

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
+"""Command line interface for brain-games."""
 import prompt
 
 
 def welcome_user():
+    """Ask user name and welcome user."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print('Hello, {n}!'.format(n=name))
