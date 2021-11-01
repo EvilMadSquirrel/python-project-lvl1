aa2bbb8ed292ba67e503e89c4af37a244985b852
EvilMadSquirrel <minichev.s.l@gmail.com>
2021-11-01 13:28:33 +0300

    finish step four

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-
+"""Main module."""
 from brain_games.cli import welcome_user
 
 
 def main():
+    """Do runs welcome string. Runs welcome script."""
     print('Welcome to the Brain Games!')
     welcome_user()
 
