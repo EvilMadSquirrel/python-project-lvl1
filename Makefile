12c9d16e65bb6878ba3c321d36b71d9d3ce8a302
EvilMadSquirrel <minichev.s.l@gmail.com>
2021-11-02 13:31:58 +0300

    finish step six

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 install:
 	poetry install
 
+uninstall:
+	pip uninstall hexlet-code
+
 brain-games:
 	poetry run brain-games
 
 brain-even:
 	poetry run brain-even
+
+brain-calc:
+	poetry run brain-calc
 
 build:
 	poetry build
---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,64 @@
+"""Calc game script."""
+import random
+
+from brain_games.game_utils import process_all
+
+
+def generate_expression():
+    """Do generate question.
+
+    Returns:
+        tuple:
+            [0], [1]: numbers for calculation
+            [2]: calculation operator +, -, or *
+    """
+    operators = ('+', '-', '*')
+    limit = 50
+    question = (
+        random.randint(0, limit),
+        random.randint(0, limit),
+        random.choice(operators),
+    )
+    q_string = 'Question: {0} {2} {1}'.format(*question)
+    print(q_string)
+    return question
+
+
+def check_expression(question, answer):
+    """Do check player answer.
+
+    Args:
+        question (tuple): Two numbers and operator
+        answer (str): Player answer
+
+    Returns:
+        bool: Correct or incorrect player answer.
+    """
+    num1, num2, operator = question
+    right_answers = {
+        '+': num1 + num2,
+        '*': num1 * num2,
+        '-': num1 - num2,
+    }
+    player_answer = 0
+    try:
+        player_answer = int(answer)
+    except ValueError:
+        print('Incorrect answer')
+        return False
+    if player_answer == right_answers[operator]:
+        print('Correct')
+        return True
+    print("'{0}' is wrong answer ;(. ".format(player_answer), end='')
+    print("Correct answer was '{0}'.".format(right_answers[operator]))
+    return False
+
+
+def main():
+    """Do launch and controls calc game."""
+    description = 'What is the result of the expression?'
+    process_all(description, generate_expression, check_expression)
+
+
+if __name__ == '__main__':
+    main()
